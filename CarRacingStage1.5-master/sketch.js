e7728dc92bde2ad60af5c942b8a288721ee5113e
temiadeife <69051649+temiadeife@users.noreply.github.com>
2020-11-13 18:50:50 -0600

    Add files via upload

---
+++ CarRacingStage1.5-master/sketch.js
@@ -0,0 +1,39 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var track, car1_Image, car2_Image, car3_Image, car4_Image
+function preload(){
+track = loadImage("../images/track.jpg");
+car1_Image = loadImage("../images/car1.png");
+car2_Image = loadImage("../images/car2.png");
+car3_Image = loadImage("../images/car3.png");
+car4_Image = loadImage("../images/car4.png");
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
